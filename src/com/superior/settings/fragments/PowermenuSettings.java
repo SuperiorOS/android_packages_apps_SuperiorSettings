ec0194a8495787a1f8a128530e1c44d256acd241
Alex Cruz <du.alexcruz@gmail.com>
2018-02-04 00:08:03 -0500

    Ensure that lockscreen power menu options the visibility is hidden
    This change will make sure that when the switch to change the visibility of the
    power menu is toggled off, the options to toggle reboot, screenshot, etc are also
    disabled. Leaving them is on is poor UX.
    
    With this change everything will work in harmony. The main switches will override the
    lockscreen switches while enabled but the lockscreen visibility switch will have overall
    control as it should.
    
    Change-Id: I15e89dec3381c59025b9a95dfdca78fe29684fed
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- src/com/superior/settings/fragments/PowermenuSettings.java
+++ src/com/superior/settings/fragments/PowermenuSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Superior OS Project
+ * Copyright (C) 2014-2016 The Dirty Unicorns Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,36 +16,98 @@
 
 package com.superior.settings.fragments;
 
-import android.content.Context;
-import android.content.ContentResolver;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
 import com.android.internal.logging.nano.MetricsProto;
 
-import com.superior.settings.R;
+public class PowerMenu extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
-public class PowermenuSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+    private static final String KEY_POWERMENU_LOCKSCREEN = "powermenu_lockscreen";
+    private static final String KEY_POWERMENU_LS_REBOOT = "powermenu_ls_reboot";
+    private static final String KEY_POWERMENU_LS_ADVANCED_REBOOT = "powermenu_ls_advanced_reboot";
+    private static final String KEY_POWERMENU_LS_SCREENSHOT = "powermenu_ls_screenshot";
+    private static final String KEY_POWERMENU_LS_AIRPLANE = "powermenu_ls_airplane";
+
+    private SwitchPreference mPowerMenuLockscreen;
+    private SwitchPreference mPowerMenuReboot;
+    private SwitchPreference mPowerMenuAdvancedReboot;
+    private SwitchPreference mPowerMenuScreenshot;
+    private SwitchPreference mPowerMenuAirplane;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.xml.superior_settings_powermenu);
-        ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mPowerMenuLockscreen = (SwitchPreference) findPreference(KEY_POWERMENU_LOCKSCREEN);
+        mPowerMenuLockscreen.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LOCKSCREEN, 1) == 1));
+        mPowerMenuLockscreen.setOnPreferenceChangeListener(this);
+
+        mPowerMenuReboot = (SwitchPreference) findPreference(KEY_POWERMENU_LS_REBOOT);
+        mPowerMenuReboot.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_REBOOT, 1) == 1));
+        mPowerMenuReboot.setOnPreferenceChangeListener(this);
+
+        mPowerMenuAdvancedReboot = (SwitchPreference) findPreference(KEY_POWERMENU_LS_ADVANCED_REBOOT);
+        mPowerMenuAdvancedReboot.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_ADVANCED_REBOOT, 0) == 1));
+        mPowerMenuAdvancedReboot.setOnPreferenceChangeListener(this);
+
+        mPowerMenuScreenshot = (SwitchPreference) findPreference(KEY_POWERMENU_LS_SCREENSHOT);
+        mPowerMenuScreenshot.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_SCREENSHOT, 0) == 1));
+        mPowerMenuScreenshot.setOnPreferenceChangeListener(this);
+
+        mPowerMenuAirplane = (SwitchPreference) findPreference(KEY_POWERMENU_LS_AIRPLANE);
+        mPowerMenuAirplane.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWERMENU_LS_AIRPLANE, 0) == 1));
+        mPowerMenuAirplane.setOnPreferenceChangeListener(this);
+
+        updateLockscreen();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mPowerMenuLockscreen) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LOCKSCREEN, value ? 1 : 0);
+            updateLockscreen();
+            return true;
+        } else if (preference == mPowerMenuReboot) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_REBOOT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowerMenuAdvancedReboot) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_ADVANCED_REBOOT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowerMenuScreenshot) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_SCREENSHOT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowerMenuAirplane) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LS_AIRPLANE, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -53,17 +115,22 @@ public class PowermenuSettings extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.SUPERIOR;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+
+    private void updateLockscreen() {
+        boolean lockscreenOptions = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.POWERMENU_LOCKSCREEN, 1) == 1;
+
+        if (lockscreenOptions) {
+            mPowerMenuReboot.setEnabled(true);
+            mPowerMenuAdvancedReboot.setEnabled(true);
+            mPowerMenuScreenshot.setEnabled(true);
+            mPowerMenuAirplane.setEnabled(true);
+        } else {
+            mPowerMenuReboot.setEnabled(false);
+            mPowerMenuAdvancedReboot.setEnabled(false);
+            mPowerMenuScreenshot.setEnabled(false);
+            mPowerMenuAirplane.setEnabled(false);
+        }
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
-    }
-}
+} 
