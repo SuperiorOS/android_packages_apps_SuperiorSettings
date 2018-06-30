1ef7fc42576b510ce170e064ca418fd38d4bc273
maxwen <max.weninger@gmail.com>
2018-06-30 14:07:37 +0530

    phoenix: Make aspect ratio hack configable on app level [2/2]
    Change-Id: I5fca5974835fb44014e09bd2a18bfdc82db52142
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- src/com/superior/settings/fragments/MiscSettings.java
+++ src/com/superior/settings/fragments/MiscSettings.java
@@ -16,10 +16,17 @@
 
 package com.superior.settings.fragments;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.text.TextUtils;
 import androidx.preference.SwitchPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -36,10 +43,25 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import com.superior.settings.R;
 
-import com.superior.settings.preferences.SystemSettingMasterSwitchPreference;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import com.superior.settings.preferences.AppMultiSelectListPreference;
+import com.superior.settings.preferences.ScrollAppsViewPreference;
 
 public class MiscSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
+    private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
+
+    private AppMultiSelectListPreference mAspectRatioAppsSelect;
+    private ScrollAppsViewPreference mAspectRatioApps;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,6 +75,31 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         Preference SmartPixels = findPreference("smart_pixels");
         if (!enableSmartPixels){
             getPreferenceScreen().removePreference(SmartPixels);
+        }
+
+        final PreferenceCategory aspectRatioCategory =
+            (PreferenceCategory) getPreferenceScreen().findPreference(KEY_ASPECT_RATIO_CATEGORY);
+        final boolean supportMaxAspectRatio =
+            getResources().getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+        if (!supportMaxAspectRatio) {
+            getPreferenceScreen().removePreference(aspectRatioCategory);
+        } else {
+            mAspectRatioAppsSelect =
+                (AppMultiSelectListPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST);
+            mAspectRatioApps =
+                (ScrollAppsViewPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST_SCROLLER);
+            final String valuesString = Settings.System.getString(getContentResolver(),
+                Settings.System.ASPECT_RATIO_APPS_LIST);
+            List<String> valuesList = new ArrayList<String>();
+            if (!TextUtils.isEmpty(valuesString)) {
+                valuesList.addAll(Arrays.asList(valuesString.split(":")));
+                mAspectRatioApps.setVisible(true);
+                mAspectRatioApps.setValues(valuesList);
+            } else {
+                mAspectRatioApps.setVisible(false);
+            }
+            mAspectRatioAppsSelect.setValues(valuesList);
+            mAspectRatioAppsSelect.setOnPreferenceChangeListener(this);
         }
 
     }
@@ -73,6 +120,21 @@ public class MiscSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mAspectRatioAppsSelect) {
+            Collection<String> valueList = (Collection<String>) newValue;
+            mAspectRatioApps.setVisible(false);
+            if (valueList != null) {
+                Settings.System.putString(getContentResolver(),
+                        Settings.System.ASPECT_RATIO_APPS_LIST, TextUtils.join(":", valueList));
+                mAspectRatioApps.setVisible(true);
+                mAspectRatioApps.setValues(valueList);
+            } else {
+                Settings.System.putString(getContentResolver(),
+                Settings.System.ASPECT_RATIO_APPS_LIST, "");
+            }
+            return true;
+        }
         return false;
     }
 }
