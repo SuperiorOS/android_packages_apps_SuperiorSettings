a3da402077fd6036825239a29084271550ccc793
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2019-01-31 15:53:01 +0530

    Destructor: Disable Flashlight blink on call by default [2/2]
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ia6ae562611a093531f3454c3f5f7ac27c2fe8d81

--- src/com/superior/settings/fragments/NotificationSettings.java
+++ src/com/superior/settings/fragments/NotificationSettings.java
@@ -59,7 +59,7 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
 	mFlashlightOnCall = (ListPreference) findPreference(FLASHLIGHT_ON_CALL);
 	Preference FlashOnCall = findPreference("flashlight_on_call");        
 	int flashlightValue = Settings.System.getInt(getContentResolver(),
-		Settings.System.FLASHLIGHT_ON_CALL, 1);
+		Settings.System.FLASHLIGHT_ON_CALL, 0);
 	mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
 	mFlashlightOnCall.setSummary(mFlashlightOnCall.getEntry());
 	mFlashlightOnCall.setOnPreferenceChangeListener(this);	
