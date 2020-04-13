940f3e87165b41399f5e178d203b425948f84f8a
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2020-04-14 03:24:01 +0530

    Revert "SuperiorSettings: FOD pressed state [2/2]"
    This reverts commit dc69dfdf0df71b7fa8868a94110f6b61863b260a.
    
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I2afc3c59f712f8bb37fde926198bb0fe9022ab95

--- src/com/superior/settings/fragments/LockscreenSettings.java
+++ src/com/superior/settings/fragments/LockscreenSettings.java
@@ -45,7 +45,7 @@ public class LockscreenSettings extends SettingsPreferenceFragment implements
 
     private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
     private static final String LOCKSCREEN_VISUALIZER_ENABLED = "lockscreen_visualizer_enabled";
-    private static final String FOD_ICON_PICKER_CATEGORY = "fod_icon_picker";
+    private static final String FOD_ICON_PICKER_CATEGORY = "fod_icon_picker_category";
 
     private SecureSettingMasterSwitchPreference mVisualizerEnabled;
     private FingerprintManager mFingerprintManager;
@@ -75,13 +75,13 @@ public class LockscreenSettings extends SettingsPreferenceFragment implements
         int visualizerEnabled = Settings.Secure.getInt(resolver,
                 LOCKSCREEN_VISUALIZER_ENABLED, 0);
         mVisualizerEnabled.setChecked(visualizerEnabled != 0);
-        }
 
         mFODIconPickerCategory = (PreferenceCategory) findPreference(FOD_ICON_PICKER_CATEGORY);
         if (mFODIconPickerCategory != null
                 && !getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView)) {
             prefScreen.removePreference(mFODIconPickerCategory);
         }
+    }
 
     @Override
     public int getMetricsCategory() {
