855050da7ba91048517a945e7ef86f7f01320d61
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2021-01-07 20:25:26 +0530

    Superiorsettings: Drop some stuffs
    - We r not Evo
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I2dba4c0536b8b89c6155af2229a4352240014e35

--- src/com/superior/settings/preferences/FODIconPicker.java
+++ src/com/superior/settings/preferences/FODIconPicker.java
@@ -69,7 +69,6 @@ public class FODIconPicker extends LayoutPreference {
     private static ImageButton Button21;
     private static ImageButton Button22;
     private static ImageButton Button23;
-    private static ImageButton Button24;
 
     private static final String TAG = "FODIconPicker";
 
@@ -133,7 +132,6 @@ public class FODIconPicker extends LayoutPreference {
         Button21 = findViewById(R.id.fodicon21_button);
         Button22 = findViewById(R.id.fodicon22_button);
         Button23 = findViewById(R.id.fodicon23_button);
-        Button24 = findViewById(R.id.fodicon24_button);
 
         int defaultfodicon = Settings.System.getInt(
                 context.getContentResolver(), Settings.System.FOD_ICON, 0);
@@ -185,8 +183,6 @@ public class FODIconPicker extends LayoutPreference {
             updateHighlightedItem(Button22, context);
         } else if (defaultfodicon == 23) {
             updateHighlightedItem(Button23, context);
-        } else if (defaultfodicon == 24) {
-            updateHighlightedItem(Button24, context);
         }
 
         Button0.setOnClickListener(new View.OnClickListener() {
@@ -357,13 +353,6 @@ public class FODIconPicker extends LayoutPreference {
                 updateHighlightedItem(Button23, context);
             }
         });
-        Button24.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                updateSettings(24, context);
-                updateHighlightedItem(Button24, context);
-            }
-        });
     }
 
     private void updateSettings(int fodicon, Context context) {
@@ -397,7 +386,6 @@ public class FODIconPicker extends LayoutPreference {
         Button21.setBackgroundTintList(defaulttint);
         Button22.setBackgroundTintList(defaulttint);
         Button23.setBackgroundTintList(defaulttint);
-        Button24.setBackgroundTintList(defaulttint);
         activebutton.setBackgroundTintList(Utils.getColorAttr(getContext(), android.R.attr.colorAccent));
     }
 }
