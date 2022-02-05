9d34cc92f1d67134511696ccec86b259c3aacc41
acras01 <acras1@gmail.com>
2022-02-05 17:46:07 +0000

    SuperiorSettings: Allow toggling combined signal icons [2/2]
    * @/jhonboy121: remove preference controller usage and use the default value for switch from sysui resource
    idoybh (YAAP): Good ol' java
    
    [maitreya29: drop unnecessary boolean definitions and minor cleanup]
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I5ab3ee84da54169ae1b43d819f2b35a92263b2f0

--- src/com/superior/settings/fragments/StatusbarSettings.java
+++ src/com/superior/settings/fragments/StatusbarSettings.java
@@ -16,35 +16,82 @@
 
 package com.superior.settings.fragments;
 
-import android.content.Context;
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.UserHandle;
 import android.content.ContentResolver;
 import android.content.res.Resources;
-import android.os.Bundle;
-import androidx.preference.SwitchPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
 import android.provider.Settings;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto;
-
 import com.android.settings.R;
 
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import android.util.Log;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+import com.superior.support.preferences.SecureSettingSwitchPreference;
+
 public class StatusbarSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+        OnPreferenceChangeListener {
+
+    private static final String COMBINED_STATUSBAR_ICONS = "show_combined_status_bar_signal_icons";
+    private static final String CONFIG_RESOURCE_NAME = "flag_combined_status_bar_signal_icons";
+    private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
+
+    SecureSettingSwitchPreference mCombinedIcons;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         addPreferencesFromResource(R.xml.superior_settings_statusbar);
-        ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefScreen = getPreferenceScreen();
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mCombinedIcons = (SecureSettingSwitchPreference)
+                findPreference(COMBINED_STATUSBAR_ICONS);
+        Resources sysUIRes = null;
+        boolean def = false;
+        int resId = 0;
+        try {
+            sysUIRes = getActivity().getPackageManager()
+                    .getResourcesForApplication(SYSTEMUI_PACKAGE);
+        } catch (Exception ignored) {
+            // If you don't have system UI you have bigger issues
+        }
+        if (sysUIRes != null) {
+            resId = sysUIRes.getIdentifier(
+                    CONFIG_RESOURCE_NAME, "bool", SYSTEMUI_PACKAGE);
+            if (resId != 0) def = sysUIRes.getBoolean(resId);
+        }
+        mCombinedIcons.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(), COMBINED_STATUSBAR_ICONS, def ? 1 : 0) == 1);
+        mCombinedIcons.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if(preference == mCombinedIcons){
+            Settings.Secure.putInt(getActivity().getContentResolver(), COMBINED_STATUSBAR_ICONS, (boolean) objValue ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -52,17 +99,4 @@ public class StatusbarSettings extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.SUPERIOR;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
-    }
 }
